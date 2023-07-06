dceae537f3d4da92f660a81be1b44adfd551004e
Rubén Freiría Iglesias <rubenfreiriaiglesias@gmail.com>
2023-07-07 00:44:08 +0200

    Items dinamicos
    Pagina que carga los items dinamicamente

---
+++ src/data/items.js
@@ -0,0 +1,75 @@
+export const items = [
+    {
+      id: 'pixel-7',
+      title: 'Pixel 7',
+      url: 'https://www.amazon.es/Google-Pixel-Factory-Unlocked-Smartphone/dp/B09GPDYMPN/ref=sr_1_1?__mk_es_ES=%C3%85M%C3%85%C5%BD%C3%95%C3%91&crid=2SJJUSKGH60DF&keywords=pixel+6&qid=1688663806&sprefix=pixel+6%2Caps%2C159&sr=8-1',
+      img: 'https://m.media-amazon.com/images/I/614Gij3tGwL._AC_SX679_.jpg',
+      specs: [
+        'Marca: Google',
+        'Nombre del modelo: Pixel 7 128GB',
+        'Operador de servicios inalámbricos: Todas las empresas de transportes',
+        'Sistema operativo: Android',
+        'Tecnología de móvil: 5G',
+        'Capacidad de almacenamiento de memoria: 8 GB',
+        'Tecnología de conectividad: Bluetooth',
+        'Color: Nieve',
+        'Tamaño de pantalla: 6,3 Pulgadas',
+        'Tecnología de red inalámbrica: GSM, LTE'
+      ]
+    },
+    {
+      id: 'samsumg-LS49AG952NPXEN',
+      title: 'SAMSUNG LS49AG952NPXEN',
+      url: 'https://www.amazon.es/SAMSUNG-LS49AG952NPXEN-Mini-LED-FreeSync-Respuesta/dp/B0BRLBTV8P/ref=sr_1_3?keywords=monitor+samsung+49+pulgadas&qid=1688664338&sprefix=monitor+samsung+49%2Caps%2C107&sr=8-3',
+      img: 'https://m.media-amazon.com/images/I/81gf+wgrcfS._AC_UL320_.jpg',
+      specs: [
+        'Tamaño de pantalla: 49 Pulgadas',
+        'Marca: SAMSUNG',
+        'Función especial: Pantalla curvada, Hub USB, Montable en pared, Ajuste de altura, Ajuste de inclinación, Ajuste giratorioPantalla curvada, Hub USB, Montable en pared, Ajuste de altura, Ajuste de inclinación, Ajuste giratorio',
+        'Frecuencia de actualización: 240 Hz, 144 Hz',
+        'Tecnología de conectividad: HDMI',
+        'Relación de aspecto: 16:9',
+        'Usos específicos del producto: Juegos',
+        'Tiempo de respuesta: 1 Milisegundos',
+        'Color: Black White',
+        'Total de puertos HDMI: 1'
+      ]
+    },
+    {
+      id: 'GIGABYTE-B550-AORUS-Elite-V2',
+      title: 'GIGABYTE B550 AORUS Elite V2',
+      url: 'https://www.amazon.es/Gigabyte-Technology-B550-AORUS-V2/dp/B08MPZ14FY/ref=sr_1_7?crid=2PFCL2GNT3RTU&keywords=placa+base&qid=1688664543&sprefix=placa+base%2Caps%2C99&sr=8-7',
+      img: 'https://m.media-amazon.com/images/I/61cwKDO98rS._AC_SX679_.jpg',
+      specs: [
+        'Marca: Gigabyte Technology',
+        'Conector de CPU: AM4',
+        'Dispositivos compatibles: Ordenador personal',
+        'Tecnología de memoria RAM: DDR4',
+        'Procesadores compatibles: ryzen',
+        'Tipo de chipset: Intel H61 Express',
+        'Velocidad de reloj de memoria: 4733 MHz',
+        'Plataforma: Windows 10',
+        'Nombre del modelo: B550 AORUS ELITE V2',
+        'Modelo de CPU: A Series A8'
+      ]
+    },
+    {
+      id: 'Ozone-gaming-battle-royale',
+      title: 'Teclado Ozone gaming battle royale',
+      url: 'https://www.amazon.es/Ozone-Gaming-OZBATTLERYLSP-Mecanico-silencioso/dp/B09BBHWPVS/ref=sr_1_4?crid=1QEVIWJXEY3ZC&keywords=teclado+ozone+tkl&qid=1688674479&sprefix=%2Caps%2C148&sr=8-4',
+      img: 'https://m.media-amazon.com/images/I/61+rY+za4wL._AC_SX679_.jpg',
+      specs: [
+        'Marca: Ozone Gaming',
+        'Dispositivos compatibles: Windows / Android / Linux / iOS',
+        'Tecnología de conectividad: USB',
+        'Descripción del teclado: Gaming',
+        'Usos recomendados para el producto: Videojuegos',
+        'Función especial: Teclas de acceso rápido y teclas multimedia',
+        'Color: Negro',
+        'Sistema operativo: Ios,Linux',
+        'Número de teclas: 19',
+        'Compatible con color de retroiluminación de teclado: RGB'
+      ]
+    }
+  ];
+  
