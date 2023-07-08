5543cf12bc4c0255c7b38fe293cc510960f62670
Rubén Freiría Iglesias <rubenfreiriaiglesias@gmail.com>
2023-07-08 02:29:32 +0200

    Build

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,11 @@
 import { defineConfig } from 'astro/config';
-
 import tailwind from "@astrojs/tailwind";
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'server',
-  site: 'https://rubenfreiria.github.io',
+  adapter: vercel(),
+  output: 'server'
 });
