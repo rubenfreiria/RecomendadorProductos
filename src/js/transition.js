ef5f5058f03383e1fba34a0cf252cfab46f02c82
Rubén Freiría Iglesias <rubenfreiriaiglesias@gmail.com>
2023-07-08 16:19:24 +0200

    Favicon y descripcion
    Se establecen favicon y descripcion y se borran lineas comentadas

--- src/js/transition.js
+++ src/js/transition.js
@@ -1,33 +1,3 @@
-/* if(document.startViewTransition){
-    window.navigation.addEventListener('navigate', (event) =>{
-        const toUrl = new URL(event.destination.url)
-
-        //Caso de que la url sea externa
-        if (location.origin !== toUrl.origin) return
-
-        //Si es el mismo dominio
-        event.intercept({
-            async handler (){
-                //Cargamos la pagina de destino
-                const response = await fetch(toUrl.pathname)
-                const text = await response.text()
-
-                //Nos quedamos solo con el contenido de la etiqueta body
-                const [, data] = text.match(/<body>([\s\S]*)<\/body>/i)
-                
-                //Ultilazamos la API de View Transition
-                document.startViewTransition(() => {
-                    // el scroll hacia arriba del todo
-                    document.body.innerHTML = data
-                    document.documentElement.scrollTop = 0
-                })
-            }
-        })
-    })
-}
- */
-
-
 if (document.startViewTransition) {
     window.navigation.addEventListener('navigate', async (event) => {
       const toUrl = new URL(event.destination.url);
