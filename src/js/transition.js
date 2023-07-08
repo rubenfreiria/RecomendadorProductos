a1bd3658cd33d17b23b8aee0241128251e50a2dc
Rubén Freiría Iglesias <rubenfreiriaiglesias@gmail.com>
2023-07-08 13:45:39 +0200

    Prueba nuevo script

--- src/js/transition.js
+++ src/js/transition.js
@@ -1,4 +1,4 @@
-if(document.startViewTransition){
+/* if(document.startViewTransition){
     window.navigation.addEventListener('navigate', (event) =>{
         const toUrl = new URL(event.destination.url)
 
@@ -24,4 +24,36 @@ if(document.startViewTransition){
             }
         })
     })
-}
+} */
+
+
+if (document.startViewTransition) {
+    window.navigation.addEventListener('navigate', async (event) => {
+      const toUrl = new URL(event.destination.url);
+  
+      // Caso de que la URL sea externa
+      if (location.origin !== toUrl.origin) return;
+  
+      // Si es el mismo dominio
+      event.intercept({
+        async handler() {
+          // Cargamos la página de destino
+          const response = await fetch(toUrl.pathname);
+          const text = await response.text();
+  
+          // Nos quedamos solo con el contenido de la etiqueta body
+          const [, data] = text.match(/<body>([\s\S]*)<\/body>/i);
+  
+          // Utilizamos la API de View Transition
+          document.startViewTransition(() => {
+            // Reemplazamos el contenido del body
+            document.body.innerHTML = data;
+  
+            // Desplazamos al inicio de la página
+            document.documentElement.scrollTop = 0;
+          });
+        },
+      });
+    });
+  }
+  
